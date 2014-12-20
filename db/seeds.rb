f9051a9e627f430376ec5584735668c891c36312
thatkent <matthkg852@gmail.com>
2014-12-21 02:03:07 +1000

    associated users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
 require 'faker'
 
+5.times do 
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10))
+  user.skip_confirmation!
+  user.save!
+end
+
+users = user.all
+
+
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -10,11 +23,18 @@ posts = Post.all
 
 100.times do 
   Comment.create!(
+    # user: users.samle,
     post: posts.sample,
     body: Faker::Lorem.paragraph
     )
   
 end
+
+user = User.first
+user.skip_reconfirmationa!
+user.update_attributes!(
+  email: 'matthkg852@gmail.com'
+  password: 'jj123123')
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
