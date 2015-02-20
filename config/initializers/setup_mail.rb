c4bb0742196aeb88a9f34341c5ce4396a99d3e43
thatkent <matthkg852@gmail.com>
2015-02-20 10:37:20 +1000

    favorites +

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development? || Rails.env.production?
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      'app32529067@heroku.com',
-    password:       'exAmplePa$$',
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
